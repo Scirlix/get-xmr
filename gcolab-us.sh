35adff6d138b2ac518c277edbb3d79e698d76276
Scirlix <scirlix@gmail.com>
2022-07-13 06:12:28 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-xmrig -o pool.minexmr.com:4444 -u 488B88tTzQmW6TgzeNfsRABh6a7V6mhBqHPXBQssWJh7XDU1K2DbeNsD2rWr267DVzWgoD8FyrkqRVzotSM7MkmK7i8ac4c --tls
+./xmrig -o pool.minexmr.com:4444 -u 488B88tTzQmW6TgzeNfsRABh6a7V6mhBqHPXBQssWJh7XDU1K2DbeNsD2rWr267DVzWgoD8FyrkqRVzotSM7MkmK7i8ac4c -k --tls --rig-id GC-US
